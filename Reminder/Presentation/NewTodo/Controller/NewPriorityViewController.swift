7909266062f2461ca40a083e61dc5acb3aa57f56
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-04 00:45:04 +0900

    [Feat] 우선순위, 태그 VC 추가

---
+++ Reminder/Presentation/NewTodo/Controller/NewPriorityViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewPriorityViewController.swift
+//  Reminder
+//
+//  Created by 강석호 on 7/3/24.
+//
+
+import UIKit
+import SnapKit
+
+final class NewPriorityViewController: BaseViewController {
+    
+}
---
+++ Reminder/Presentation/NewTodo/Controller/NewTagViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewTagViewController.swift
+//  Reminder
+//
+//  Created by 강석호 on 7/3/24.
+//
+
+import UIKit
+import SnapKit
+
+final class NewTagViewController: BaseViewController {
+    
+}
