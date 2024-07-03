65467a3f3443d45e28fa7a0a1a3212ef33365503
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-04 00:44:03 +0900

    [Feat] 새로운 할일 마감일 데이터 realm에 추가

---
+++ Reminder/Presentation/NewTodo/Controller/NewTodoViewController.swift
@@ -0,0 +1,134 @@
+//
+//  NewTodoViewController.swift
+//  Reminder
+//
+//  Created by 강석호 on 7/2/24.
+//
+
+import UIKit
+import SnapKit
+import RealmSwift
+
+final class NewTodoViewController: BaseViewController {
+
+    let tableView = UITableView(frame: .zero, style: .insetGrouped)
+    private let todoInputView = TodoInputView()
+    private var realm: Realm!
+    private var selectedDeadline: Date?
+
+    private let sectionTitles = ["할 일 입력", "마감일", "태그", "우선 순위", "이미지 추가"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigationBar()
+        configureRealm()
+        configureTableView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+
+    private func configureRealm() {
+        realm = try! Realm()
+    }
+
+    private func setupNavigationBar() {
+        navigationItem.title = "새로운 할 일"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "추가", style: .done, target: self, action: #selector(addTodo))
+        navigationItem.rightBarButtonItem?.isEnabled = false
+        navigationItem.rightBarButtonItem?.tintColor = UIColor.systemBlue
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(dismissViewController))
+    }
+
+    private func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.register(TodoInputViewCell.self, forCellReuseIdentifier: "TodoInputViewCell")
+    }
+    
+    override func configureHierarchy() {
+        view.addSubview(tableView)
+    }
+    
+    override func configureConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    override func configureTarget() {
+        todoInputView.titleTextField.addTarget(self, action: #selector(updateAddButtonState), for: .editingChanged)
+    }
+
+    @objc private func updateAddButtonState() {
+        let isNotEmpty = !(todoInputView.titleTextField.text?.isEmpty ?? true)
+        navigationItem.rightBarButtonItem?.isEnabled = isNotEmpty
+        navigationItem.rightBarButtonItem?.tintColor = isNotEmpty ? UIColor.systemBlue : UIColor.gray
+    }
+
+    @objc private func addTodo() {
+        let newTodo = TodoTable()
+        try! realm.write {
+            newTodo.title = todoInputView.titleTextField.text ?? ""
+            newTodo.memo = todoInputView.memoTextView.text.isEmpty ? nil : todoInputView.memoTextView.text
+            newTodo.deadline = selectedDeadline
+            realm.add(newTodo)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+
+    @objc private func dismissViewController() {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension NewTodoViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionTitles.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TodoInputViewCell", for: indexPath) as! TodoInputViewCell
+            cell.setup(todoInputView: todoInputView)
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = sectionTitles[indexPath.section]
+            cell.accessoryType = .disclosureIndicator
+            
+            if indexPath.section == 1, let deadline = selectedDeadline {
+                print("++++++++++++++")
+//                let formatter = DateFormatter()
+//                formatter.dateStyle = .medium
+//                cell.detailTextLabel?.text = formatter.string(from: deadline)
+//                cell.detailTextLabel?.text = "aaa"
+            }
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        if indexPath.section == 1 {
+            let deadlineVC = NewDeadlineViewController()
+            deadlineVC.onSave = { [weak self] date in
+                self?.selectedDeadline = date
+                self?.tableView.reloadRows(at: [indexPath], with: .none)
+            }
+            navigationController?.pushViewController(deadlineVC, animated: true)
+        }
+    }
+}
