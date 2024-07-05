0eed81b618bbadc9f5a6a416f4ec052d3f13b86c
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-05 15:40:45 +0900

    [Feat] TodoTable 태그, 우선순위 column 추가

--- Reminder/DataBase/Todo.swift
+++ Reminder/DataBase/Todo.swift
@@ -12,4 +12,16 @@ class TodoTable: Object {
     @Persisted var title: String
     @Persisted var memo: String?
     @Persisted var deadline: Date?
+    @Persisted var tag: String?
+    @Persisted var priority: String?
+
+    
+    convenience init(title: String, memo: String?, deadline: Date?, tag: String?, priority: String?) {
+        self.init()
+        self.title = title
+        self.memo = memo
+        self.deadline = deadline
+        self.tag = tag
+        self.priority = priority
+    }
 }
