6db4dbe2f5d822458568d57ed043d38220669cc7
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-05 17:10:48 +0900

    [Feat] 우선순위VC 기능구현

--- Reminder/Presentation/NewTodo/Controller/NewPriorityViewController.swift
+++ Reminder/Presentation/NewTodo/Controller/NewPriorityViewController.swift
@@ -10,4 +10,82 @@ import SnapKit
 
 final class NewPriorityViewController: BaseViewController {
     
+    let tableView = UITableView()
+    let priorityList = ["높음", "중간", "낮음"]
+    var selectedIndexPath: IndexPath?
+    let saveButton = UIButton()
+    var onSave: ((String) -> Void)?
+    var selectedPriority: String = "높음"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureTableView()
+    }
+    
+    override func configureHierarchy() {
+        view.addSubview(tableView)
+        view.addSubview(saveButton)
+    }
+    
+    private func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    override func configureView() {
+        saveButton.setTitle("추가하기", for: .normal)
+        saveButton.setTitleColor(.white, for: .normal)
+        saveButton.backgroundColor = .blue
+        saveButton.layer.cornerRadius = 10
+    }
+    
+    override func configureTarget() {
+        saveButton.addTarget(self, action: #selector(saveButtonClicked), for: .touchUpInside)
+    }
+    
+    override func configureConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(100)
+        }
+        saveButton.snp.makeConstraints { make in
+            make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide).inset(40)
+            make.height.equalTo(50)
+        }
+    }
+    
+    @objc private func saveButtonClicked() {
+        onSave?(selectedPriority)
+        navigationController?.popViewController(animated: true)
+    }
+}
+
+extension NewPriorityViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return priorityList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = priorityList[indexPath.row]
+        if selectedIndexPath == indexPath {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let prevIndexPath = selectedIndexPath {
+            let prevCell = tableView.cellForRow(at: prevIndexPath)
+            prevCell?.accessoryType = .none
+        }
+        let selectedCell = tableView.cellForRow(at: indexPath)
+        selectedCell?.accessoryType = .checkmark
+        selectedIndexPath = indexPath
+        selectedPriority = priorityList[indexPath.row]
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
