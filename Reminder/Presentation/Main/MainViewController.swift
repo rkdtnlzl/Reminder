695ff1abf2f0f8376951a2be37860cb641647e30
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-05 15:41:27 +0900

    [Feat] MainVC CollectionView 추가

--- Reminder/Presentation/Main/MainViewController.swift
+++ Reminder/Presentation/Main/MainViewController.swift
@@ -11,18 +11,39 @@ import SnapKit
 final class MainViewController: BaseViewController {
     
     private let newTodoButton = UIButton()
+    private let totalLabel = UILabel()
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout())
     
     override func configureHierarchy() {
         view.addSubview(newTodoButton)
+        view.addSubview(totalLabel)
+        view.addSubview(collectionView)
     }
     
     override func configureView() {
         newTodoButton.setImage(UIImage(systemName: "plus"), for: .normal)
         newTodoButton.setTitle("새로운 할 일", for: .normal)
         newTodoButton.setTitleColor(.systemBlue, for: .normal)
+        
+        totalLabel.font = .boldSystemFont(ofSize: 25)
+        totalLabel.text = "전체"
+        totalLabel.textColor = .gray
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(MainTodoCell.self, forCellWithReuseIdentifier: MainTodoCell.id)
     }
     
     override func configureConstraints() {
+        totalLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(totalLabel.snp.bottom).offset(10)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.bottom.equalTo(newTodoButton.snp.top).inset(-20)
+        }
         newTodoButton.snp.makeConstraints { make in
             make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide).inset(40)
             make.height.equalTo(50)
@@ -39,5 +60,36 @@ final class MainViewController: BaseViewController {
         navVC.modalPresentationStyle = .pageSheet
         present(navVC, animated: true)
     }
+    
+    func collectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        let width = UIScreen.main.bounds.width - 40
+        layout.itemSize = CGSize(width: width/2-10, height: width/3-10)
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 10
+        layout.minimumLineSpacing = 10
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+        return layout
+    }
 }
 
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MainTodoCell.id, for: indexPath) as? MainTodoCell else {
+            return UICollectionViewCell()
+        }
+        cell.backgroundColor = .lightGray
+        cell.layer.cornerRadius = 10
+        
+        if indexPath.row == 0 {
+            cell.categoryTitle.text = "전체"
+        }
+        return cell
+    }
+    
+    
+}
