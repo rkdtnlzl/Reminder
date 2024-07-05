c207a3415f9dd787d3f286f20bdd7ba099eed42f
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-05 23:44:20 +0900

    [Feat] Realm Repository Pattern 적용

---
+++ Reminder/DataBase/TodoTableRepository.swift
@@ -0,0 +1,36 @@
+//
+//  TodoTableRepository.swift
+//  Reminder
+//
+//  Created by 강석호 on 7/5/24.
+//
+
+import Foundation
+import RealmSwift
+
+final class PurchaseTableRepository {
+    
+    private let realm = try! Realm()
+    
+    func createItem(_ data: TodoTable) {
+        
+        do {
+            try realm.write {
+                realm.add(data)
+                print("Realm Create Success")
+            }
+        } catch {
+            print("Realm Error")
+        }
+    }
+    
+    func countItems() -> Int {
+        return realm.objects(TodoTable.self).count
+    }
+    
+    func deleteItem(_ data: TodoTable) {
+        try! realm.write {
+            realm.delete(data)
+        }
+    }
+}
