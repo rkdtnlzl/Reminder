1de25e00d76b7e3d11f20cfea0a3781e43cec114
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-08 22:07:13 +0900

    [Feat] Realm 폴더 레포지토리 추가

---
+++ Reminder/DataBase/FolderRepository.swift
@@ -0,0 +1,33 @@
+//
+//  FolderRepository.swift
+//  Reminder
+//
+//  Created by 강석호 on 7/8/24.
+//
+
+import Foundation
+import RealmSwift
+
+final class FolderRepository {
+    private let realm = try! Realm()
+    
+    func fetchAllFolders() -> Results<Folder> {
+        return realm.objects(Folder.self)
+    }
+
+    func addFolder(to todo: TodoTable) {
+        guard let tag = todo.tag else { return }
+        
+        if let existingFolder = realm.objects(Folder.self).filter("folderName == %@", tag).first {
+            try! realm.write {
+                existingFolder.folderList.append(todo)
+            }
+        } else {
+            let newFolder = Folder(tag: tag)
+            try! realm.write {
+                realm.add(newFolder)
+                newFolder.folderList.append(todo)
+            }
+        }
+    }
+}
