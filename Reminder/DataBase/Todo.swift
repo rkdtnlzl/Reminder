5432f68f5b9eefb40a1bb853e3a5d2606473be6f
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-08 22:08:02 +0900

    [Feat] 폴더 Realm Table 추가 -> TodoTable LinkingObjects

--- Reminder/DataBase/Todo.swift
+++ Reminder/DataBase/Todo.swift
@@ -8,6 +8,19 @@
 import RealmSwift
 import Foundation
 
+class Folder: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var folderList: List<TodoTable>
+    @Persisted var folderName: String
+    @Persisted var folderImage: String
+
+    convenience init(tag: String) {
+        self.init()
+        self.folderName = tag
+        self.folderImage = "star"
+    }
+}
+
 class TodoTable: Object {
     @Persisted(primaryKey: true) var id: ObjectId
     @Persisted var title: String
@@ -15,7 +28,9 @@ class TodoTable: Object {
     @Persisted var deadline: Date?
     @Persisted var tag: String?
     @Persisted var priority: String?
-
+    
+    @Persisted(originProperty: "folderList")
+    var main: LinkingObjects<Folder>
     
     convenience init(title: String, memo: String?, deadline: Date?, tag: String?, priority: String?) {
         self.init()
