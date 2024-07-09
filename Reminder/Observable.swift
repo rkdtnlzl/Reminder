2ed40e62bc4ef86ec771a6ec083181b1d387c090
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-09 17:21:41 +0900

    [Feat] Observable 객체 생성

---
+++ Reminder/Observable.swift
@@ -0,0 +1,26 @@
+//
+//  Observable.swift
+//  Reminder
+//
+//  Created by 강석호 on 7/9/24.
+//
+
+import Foundation
+
+class Observable<T> {
+    var closure: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            closure?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(closure: @escaping (T) -> Void) {
+        self.closure = closure
+    }
+}
