6de610c0da78e5038a550d7ed0ad1bd7df0a3159
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-09 17:22:28 +0900

    [Feat] 바인딩을 통해 데이터 반응

--- Reminder/Presentation/NewTodo/Controller/NewDeadlineViewController.swift
+++ Reminder/Presentation/NewTodo/Controller/NewDeadlineViewController.swift
@@ -14,6 +14,22 @@ final class NewDeadlineViewController: BaseViewController {
     private let datePicker = UIDatePicker()
     private let saveButton = UIButton()
     var onSave: ((Date) -> Void)?
+    private let dateLabel = UILabel()
+    let viewModel = NewDeadlineViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindData()
+    }
+    
+    func bindData() {
+        viewModel.outputDateLabel.bind { date in
+            let myFormatter = DateFormatter()
+            myFormatter.dateFormat = "yyyy-MM-dd"
+            let dateString = myFormatter.string(from: date!)
+            self.dateLabel.text = dateString
+        }
+    }
     
     override func configureView() {
         datePicker.datePickerMode = .date
@@ -24,15 +40,23 @@ final class NewDeadlineViewController: BaseViewController {
         saveButton.setTitleColor(.white, for: .normal)
         saveButton.backgroundColor = .blue
         saveButton.layer.cornerRadius = 10
+        
+        dateLabel.text = "test"
+        dateLabel.font = .boldSystemFont(ofSize: 17)
+        dateLabel.backgroundColor = .gray
+        dateLabel.textColor = .black
+        dateLabel.textAlignment = .center
     }
     
     override func configureTarget() {
         saveButton.addTarget(self, action: #selector(saveButtonClicked), for: .touchUpInside)
+        datePicker.addTarget(self, action: #selector(changeDate), for: .valueChanged)
     }
     
     override func configureHierarchy() {
         view.addSubview(datePicker)
         view.addSubview(saveButton)
+        view.addSubview(dateLabel)
     }
     
     override func configureConstraints() {
@@ -44,10 +68,19 @@ final class NewDeadlineViewController: BaseViewController {
             make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide).inset(40)
             make.height.equalTo(50)
         }
+        
+        dateLabel.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(40)
+            make.bottom.equalTo(saveButton.snp.top).inset(-30)
+        }
     }
     
     @objc private func saveButtonClicked() {
         onSave?(datePicker.date)
         navigationController?.popViewController(animated: true)
+    }
+    
+    @objc private func changeDate() {
+        viewModel.inputDateUpdate.value = datePicker.date
     }
 }
