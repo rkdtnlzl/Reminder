8e9e540db30d6afe1cc2a8a2439570c00f80d1fb
rkdtnlzl <rkdtnlzl@naver.com>
2024-07-09 21:45:04 +0900

    [Feat] 우선순위ViewModel 생성

---
+++ Reminder/Presentation/NewTodo/ViewModel/NewPriorityViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  NewPriorityViewModel.swift
+//  Reminder
+//
+//  Created by 강석호 on 7/9/24.
+//
+
+import Foundation
+
+class NewPriorityViewModel {
+    
+    var inputPriorityIndex = Observable(0)
+    
+    var outputPriorityLabel = Observable("")
+    
+    init() {
+        inputPriorityIndex.bind { _ in
+            self.updatePriority()
+        }
+    }
+    
+    private func updatePriority() {
+        
+        if inputPriorityIndex.value == 0 {
+            outputPriorityLabel.value = "높음"
+        } else if inputPriorityIndex.value == 1 {
+            outputPriorityLabel.value = "중간"
+        } else {
+            outputPriorityLabel.value = "낮음"
+        }
+    }
+}
